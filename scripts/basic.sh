9994c39ae121b89e99ac224a56b159e5e1d414e7
Tawan 'catflyflyfly' Wattana <c.catfly@gmail.com>
2022-06-21 20:54:19 +0700

    feat: add train args parser

---
+++ scripts/basic.sh
@@ -0,0 +1,3 @@
+cargo run --\
+  --train alpha,2,1\
+  --train beta,2,2\
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap()]
+struct Args {
+    #[clap(long, value_parser = args_parser::parse_train)]
+    pub train: Option<Vec<Train>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Train {
+    pub name: String,
+    pub cap: u32,
+    pub initial_station_name: String,
+}
+
+pub mod args_parser {
+    use anyhow::{anyhow, bail};
+    use itertools::Itertools;
+
+    use crate::Train;
+
+    pub fn parse_train(input: &str) -> Result<Train, anyhow::Error> {
+        if let [name, capacity, initial_station_name] = input.split(",").collect_vec()[..] {
+            Ok(Train {
+                name: name.to_string(),
+                cap: capacity.parse().map_err(|error| {
+                    anyhow!("parse capacity `{capacity}` fail with error `{error}`")
+                })?,
+                initial_station_name: initial_station_name.to_string(),
+            })
+        } else {
+            bail!("[NAME],[CAPACITY],[INITIAL_STATION_NAME]")
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:#?}", args);
 }
