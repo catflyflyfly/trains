862f9b012680e7e49736c54a4c484d734089bed1
Tawan 'catflyflyfly' Wattana <c.catfly@gmail.com>
2022-06-23 03:44:30 +0700

    chore: split module; main now return solution

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ fn main() -> Result<()> {
     let network_args = args::Network::parse();
     let network = model::Network::try_from(network_args)?;
 
-    println!("{:#?}", network);
+    println!("{:#?}", network.optimal_instructions());
+    println!("{:#?}", network.optimal_time_mins());
 
     Ok(())
 }
